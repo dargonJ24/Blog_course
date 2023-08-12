c07f354c682c80d9f480b21743920e3c591292d9
dargonj24 <dargonj24@gmail.com>
2023-08-12 13:15:54 +0700

    bug lookup view home

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app=express()
-const port =3000
-app.get("/",(req,res)=>{
-    var a=5
-    var b=6
-    var c= a+b
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
-    res.send("hello world")
-})
-app.listen(port,console.log(`http://localhost:${port}`))
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+// Set the view engine
+app.engine(
+  'handlebars',
+  exphbs({
+    extname: '.hbs' // Specify the file extension for Handlebars templates
+  })
+);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render("home")
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
