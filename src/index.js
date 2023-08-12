05d75670dd4c2eb8b8a09ca15b3458ef67f34d6a
dargonj24 <dargonj24@gmail.com>
2023-08-12 16:22:23 +0700

    handlebars -v1  use .handlebars

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { engine } from 'express-handlebars';
+import path from 'path';
 
 const app = express();
 const port = 3000;
-
-app.use(morgan('combined'));
-
-// Set the view engine
-app.engine(
-  'handlebars',
-  exphbs({
-    extname: '.hbs' // Specify the file extension for Handlebars templates
-  })
-);
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'));
-
+const url_1="src\resources\views\home.hbs"
+app.set('views', path.join(dirname(fileURLToPath(import.meta.url)), './resources/views'));
+console.log(path.join(dirname(fileURLToPath(import.meta.url)), './resources/views'))
 app.get('/', (req, res) => {
-  res.render("home")
+    res.render('home');
 });
-
+app.get('news',(req,res)=>{
+    res.render("news")
+})
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
