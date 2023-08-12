a442ecd4981232e0004f1b4d4a043c97f5f7f6ad
dargonj24 <dargonj24@gmail.com>
2023-08-12 17:22:57 +0700

    static file && scss

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ import path from 'path';
 
 const app = express();
 const port = 3000;
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+//seup static
+app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), './public')))
+//set up handlebar
+app.engine('hbs', engine({
+    extname:'.hbs'
+}));
+
+app.set('view engine', 'hbs');
 const url_1="src\resources\views\home.hbs"
 app.set('views', path.join(dirname(fileURLToPath(import.meta.url)), './resources/views'));
 console.log(path.join(dirname(fileURLToPath(import.meta.url)), './resources/views'))
 app.get('/', (req, res) => {
     res.render('home');
 });
-app.get('news',(req,res)=>{
+app.get('/news',(req,res)=>{
     res.render("news")
 })
 app.listen(port, () => {
