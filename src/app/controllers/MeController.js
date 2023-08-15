af2f381f528a2a1ea180b07cf3c52fc07970f120
dargonj24 <dargonj24@gmail.com>
2023-08-15 23:17:33 +0700

    page my all course

---
+++ src/app/controllers/MeController.js
@@ -0,0 +1,19 @@
+import { mongooseToObject, multipleMongooseToObject } from "../../until/mongoose.js";
+import course from "../models/Course.js"
+class  MeController{
+    //[GET]/me/course/store
+    storeCourse(req,res,next){
+        course.find({}).
+        then(course=>res.render("me/storeCourse",
+       { course: multipleMongooseToObject(course)}
+        )).
+        catch(next)
+
+        
+    }
+    //
+    index(req,res){
+        res.send("hello you")
+    }
+}
+export default MeController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import newsRouter from "./news.js";
 import SitesRouter from "./site.js";
 import CourseRouter from "./course.js";
+import meRouter from "./me.js";
 export function route(app) {
+  app.use('/me',meRouter)
   app.use('/courses',CourseRouter)
     app.use('/news',newsRouter)
     app.use('/',SitesRouter)
